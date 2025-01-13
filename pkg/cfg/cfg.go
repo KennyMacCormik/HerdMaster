10fad928f97ab49131c262e1c3102b9550b9a9a4
Kenny <old.good.mail@gmail.com>
2025-01-13 03:17:29 +0300

    added grpc conf

--- pkg/cfg/cfg.go
+++ pkg/cfg/cfg.go
@@ -8,12 +8,12 @@ import (
 func init() {
 	viper.SetEnvPrefix("hm")
 	setLoggingEnv()
-
 	viper.AutomaticEnv()
 }
 
 type DefaultConfig struct {
-	Log LoggingConfig `mapstructure:",squash"`
+	Log  LoggingConfig `mapstructure:",squash"`
+	Grpc GrpcConfig    `mapstructure:",squash"`
 }
 
 type LoggingConfig struct {
@@ -21,6 +21,21 @@ type LoggingConfig struct {
 	Format string `mapstructure:"log_format" validate:"oneof=text json"`
 	// HM_LOG_LEVEL. Default info
 	Level string `mapstructure:"log_level" validate:"oneof=debug info warn error"`
+}
+
+type GrpcConfig struct {
+	// HM_GRPC_HOST. Default text
+	Host string `mapstructure:"grpc_host" validate:"ip4_addr|hostname_rfc1123"`
+	// HM_GRPC_PORT. Default info
+	Port string `mapstructure:"grpc_port" validate:"numeric,gt=1024,lt=65536"`
+}
+
+func setGrpcEnv() {
+	viper.SetDefault("grpc_host", "0.0.0.0")
+	_ = viper.BindEnv("grpc_host")
+
+	viper.SetDefault("grpc_port", "8080")
+	_ = viper.BindEnv("grpc_port")
 }
 
 func setLoggingEnv() {
