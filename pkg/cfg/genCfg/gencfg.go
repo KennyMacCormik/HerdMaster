6391f3882a1fb27b2233bd40f8cb25a2b00315c9
Kenny <old.good.mail@gmail.com>
2025-01-14 16:12:50 +0300

    sanity fixes for genCfgs

--- pkg/cfg/genCfg/gencfg.go
+++ pkg/cfg/genCfg/gencfg.go
@@ -75,17 +75,20 @@
 package genCfg
 
 // GrpcConfig represents the configuration for gRPC servers.
-// - Host: The IP address or hostname of the gRPC server. Validates as IPv4 or hostname (RFC1123).
-// - Port: The port number for the gRPC server. Validates as a numeric value between 1025 and 65 535 (exclusive).
+// Host: The IP address or hostname of the gRPC server.
+// Validates as IPv4 or hostname (RFC1123).
+// Port: The port number for the gRPC server.
+// Validates as a numeric value between 1025 and 65 535 (exclusive).
 type GrpcConfig struct {
-	Host string `mapstructure:"grpc_host" validate:"ip4_addr|hostname_rfc1123"` // GRPC_HOST. Default text
-	Port string `mapstructure:"grpc_port" validate:"numeric,gt=1024,lt=65536"`  // GRPC_PORT. Default info
+	Host string `mapstructure:"grpc_host" validate:"ip4_addr|hostname_rfc1123,required"`
+	Port string `mapstructure:"grpc_port" validate:"numeric,gt=1024,lt=65536,required"`
 }
 
 // LoggingConfig represents the configuration for logging systems.
-// - Format: Specifies the log format, either "text" or "json".
-// - Level: Specifies the log level, which must be one of "debug", "info", "warn", or "error".
+// In particular, log package from this repo.
+// Format: Specifies the log format, either "text" or "json".
+// Level: Specifies the log level, which must be one of "debug", "info", "warn", or "error".
 type LoggingConfig struct {
-	Format string `mapstructure:"log_format" validate:"oneof=text json"`            // LOG_FORMAT. Default text
-	Level  string `mapstructure:"log_level" validate:"oneof=debug info warn error"` // LOG_LEVEL. Default info
+	Format string `mapstructure:"log_format" validate:"oneof=text json"`
+	Level  string `mapstructure:"log_level" validate:"oneof=debug info warn error"`
 }
